559e291ff34ff2b71220667049f4869497eb82fe
vanthao113 <testkenmen@gmail.com>
2021-12-17 11:41:23 +0700

    first commit

---
+++ include/header.php
@@ -0,0 +1,30 @@
+<div>
+      <div class="container-fluid">
+        <div class="row nenxanh">
+                <div class="col-md-4">
+                    <img class="logo" src="images/logodhv.png" alt="">
+                </div>
+                <div class="col-md-5     cantop"><center>
+                        <h5> KÝ TÚC XÁ - ĐẠI HỌC VINH</h5>
+                        <h7></h7></center>
+                </div>
+                <div class="col-md-3 cantop"><center>
+                        <img class="logofb" src="images/logofb.png">
+                </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <nav class="menu">
+                    <ul>
+                        <li><a  href="#">TRANG CHỦ</a></li>
+                        <li><a  href="#">Webiste Trường</a></li>
+                        <li><a  href="#">Trang sinh viên Trường</a></li>
+                        <li><a href="#">Liên hệ</a></li>
+                        <li><a class="ad" href="index.php?action=login">Đăng nhập</a></li>
+                    </ul>
+                </nav>
+            </div>          
+        </div>
+    </div> 
+</div>
+ 
